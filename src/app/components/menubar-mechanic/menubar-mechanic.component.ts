d0ceb7c0a4c986ef7ec18f89838cca79f4e9622d
Manitra <94074919+ShibaMiyuki07@users.noreply.github.com>
2025-03-22 09:54:20 +0300

    Change design menubar and adding all the css files

--- src/app/components/menubar-mechanic/menubar-mechanic.component.ts
+++ src/app/components/menubar-mechanic/menubar-mechanic.component.ts
@@ -5,7 +5,17 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './menubar-mechanic.component.html',
-  styleUrl: './menubar-mechanic.component.css'
+  styleUrls: [
+    './menubar-mechanic.component.css',
+    'assets/css/font_awesome_all.css',
+    'assets/css/flaticon.css',
+    'assets/css/icomoon.css',
+    'assets/css/bootstrap.css',
+    'assets/css/global.css',
+    'assets/css/element_css/header_upper.css',
+    'assets/css/element_css/header_lower.css',
+    'assets/css/style.css',
+  ]
 })
 export class MenubarMechanicComponent {
 
