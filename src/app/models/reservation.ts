d88887ba93f801a3b6a1d108d64ef6a3c443a239
Manitra <94074919+ShibaMiyuki07@users.noreply.github.com>
2025-03-22 09:40:02 +0300

    initialisation Reservation model

---
+++ src/app/models/reservation.ts
@@ -0,0 +1,11 @@
+export interface Reservation {
+  _id : any,
+  userId : any,
+  mechanicId : any,
+  serviceId : any,
+  status : string,
+  endReservation : any,
+  reservationDate : any,
+  createdAt : any,
+  updatedAt : any,
+}
