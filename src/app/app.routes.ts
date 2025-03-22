2da87ee29ef93c12fd0e6100ec8302e2bc4b38c5
Manitra <94074919+ShibaMiyuki07@users.noreply.github.com>
2025-03-22 09:55:08 +0300

    adding index to route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {IndexMechanicComponent} from "./page/index-mechanic/index-mechanic.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: IndexMechanicComponent },
+];
