8b1b98fd7612268e147857162dae64cc932868f5
Manitra <94074919+ShibaMiyuki07@users.noreply.github.com>
2025-03-22 09:45:58 +0300

    initialisation Mechanic menubar

---
+++ src/app/components/menubar-mechanic/menubar-mechanic.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menubar-mechanic',
+  standalone: true,
+  imports: [],
+  templateUrl: './menubar-mechanic.component.html',
+  styleUrl: './menubar-mechanic.component.css'
+})
+export class MenubarMechanicComponent {
+
+}
