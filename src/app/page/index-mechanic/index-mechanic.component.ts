0691de7c7deaab765c46baee9609e92f11cc7cd0
Manitra <94074919+ShibaMiyuki07@users.noreply.github.com>
2025-03-22 09:45:03 +0300

    initialisation Mechanic index page

---
+++ src/app/page/index-mechanic/index-mechanic.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-index-mechanic',
+  standalone: true,
+  imports: [],
+  templateUrl: './index-mechanic.component.html',
+  styleUrl: './index-mechanic.component.css'
+})
+export class IndexMechanicComponent {
+
+}
