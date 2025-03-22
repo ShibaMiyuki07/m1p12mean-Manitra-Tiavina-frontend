b2bb0a1014b66553f7877e2b5401f0ffa3ee3ee0
Manitra <94074919+ShibaMiyuki07@users.noreply.github.com>
2025-03-22 12:54:32 +0300

    Add date change in the agenda

---
+++ src/app/models/Constant.ts
@@ -0,0 +1,17 @@
+export class Constant {
+  readonly monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ]
+}
+
+
--- src/app/page/index-mechanic/index-mechanic.component.ts
+++ src/app/page/index-mechanic/index-mechanic.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {MenubarMechanicComponent} from "../../components/menubar-mechanic/menubar-mechanic.component";
+import {Constant} from "../../models/Constant";
 
 @Component({
   selector: 'app-index-mechanic',
@@ -11,5 +12,23 @@ import {MenubarMechanicComponent} from "../../components/menubar-mechanic/menuba
   styleUrl: './index-mechanic.component.css'
 })
 export class IndexMechanicComponent {
+  todayDate : Date = new Date();
 
+
+  nextDate()
+  {
+    this.todayDate.setDate(this.todayDate.getDate() + 1);
+  }
+
+  prevDate()
+  {
+    this.todayDate.setDate(this.todayDate.getDate() - 1);
+  }
+
+  displayDate()
+  {
+    return this.todayDate.getDate() + ' ' + this.Constant.monthNames[this.todayDate.getMonth()] + ' ' + this.todayDate.getFullYear();
+  }
+
+  protected readonly Constant = new Constant();
 }
