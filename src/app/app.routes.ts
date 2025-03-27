760b191c595b4db948be93afeed0a1a5c0166d4c
Manitra <94074919+ShibaMiyuki07@users.noreply.github.com>
2025-03-27 20:49:33 +0300

    replace the path for index manager

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { LoginComponent } from './page/login/login.component';
 import { RegisterComponent } from './page/register/register.component';
 import { HomeComponent } from './page/home/home.component';
 import {AuthGuard} from "./core/guards/auth.guard";
+import {IndexManagerComponent} from "./page/manager/index-manager/index-manager.component";
 
 export const routes: Routes = [
   { path: 'mechanic', component: IndexMechanicComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: '', component: HomeComponent },
+  { path: 'manager' , component : IndexManagerComponent},
 ];
