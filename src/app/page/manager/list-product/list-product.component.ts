64c9fcfddde9dadeb91c3eeb1cb1fbcadc7221cb
Manitra <94074919+ShibaMiyuki07@users.noreply.github.com>
2025-03-30 21:20:55 +0300

    add click function to delete and update

--- src/app/page/manager/list-product/list-product.component.ts
+++ src/app/page/manager/list-product/list-product.component.ts
@@ -1,8 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {MenubarManagerComponent} from "../../../components/menubar-manager/menubar-manager.component";
 import {ApiProductServiceService} from "../../../services/productApi/api-product-service.service";
 import {ProductStock} from "../../../models/apiResult/product-stock";
 import {NgForOf, NgIf} from "@angular/common";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-list-product',
@@ -17,8 +18,10 @@ import {NgForOf, NgIf} from "@angular/common";
 })
 export class ListProductComponent implements OnInit {
   products: ProductStock[] = [];
+  private router = inject(Router);
+  private apiProduct = inject(ApiProductServiceService);
 
-  constructor(private apiProduct : ApiProductServiceService) {}
+  constructor() {}
 
   ngOnInit(): void {
     this.apiProduct.getAllProductsWithStocks().subscribe(products => {
@@ -26,6 +29,16 @@ export class ListProductComponent implements OnInit {
     });
   }
 
+  delete(productId : any)
+  {
+    this.apiProduct.deleteProduct(productId);
+    //To Add delete stock when it is finished
+  }
+
+  linkUpdate(productId : any)
+  {
+    this.router.navigate(['/manager/products/'+productId]);
+  }
 
 
 }
