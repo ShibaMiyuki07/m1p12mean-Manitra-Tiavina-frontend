5f405112ec1d6afc311e082c3426ba5552810428
Manitra <94074919+ShibaMiyuki07@users.noreply.github.com>
2025-04-03 18:21:04 +0300

    client product list start

---
+++ src/app/page/client/client-product-list/client-product-list.component.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+import {HeaderComponent} from "../../../components/header/header.component";
+import {LoaderComponent} from "../../../components/loader/loader.component";
+
+@Component({
+  selector: 'app-client-product-list',
+  standalone: true,
+  imports: [
+    HeaderComponent,
+    LoaderComponent
+  ],
+  templateUrl: './client-product-list.component.html',
+  styleUrls: ['./client-product-list.component.css',
+    '../../../assets-bosh/css/font_awesome_all.css',
+    '../../../assets-bosh/css/flaticon.css',
+    '../../../assets-bosh/css/icomoon.css',
+    '../../../assets-bosh/css/owl.css',
+    '../../../assets-bosh/css/bootstrap.css',
+    '../../../assets-bosh/css/jquery.fancybox.min.css',
+    '../../../assets-bosh/css/animate.css',
+    '../../../assets-bosh/css/hover.css',
+    '../../../assets-bosh/css/global.css',
+    '../../../assets-bosh/css/nice_select.css',
+    '../../../assets-bosh/css/progresscircle.css',
+    '../../../assets-bosh/css/jquery_ui.css',
+    '../../../assets-bosh/css/elpath.css',
+    '../../../assets-bosh/css/hover.css',
+    '../../../assets-bosh/css/element_css/preloader.css',
+    '../../../assets-bosh/css/element_css/time-travel.css',
+    '../../../assets-bosh/css/element_css/categories.css',
+    '../../../assets-bosh/css/element_css/instagram.css',
+    '../../../assets-bosh/css/element_css/dot_style_one.css',
+    '../../../assets-bosh/css/element_css/owl_nav_one.css',
+    '../../../assets-bosh/css/element_css/pagination.css',
+    '../../../assets-bosh/css/element_css/search_popup.css',
+    '../../../assets-bosh/css/element_css/header_top.css',
+    '../../../assets-bosh/css/element_css/header_upper.css',
+    '../../../assets-bosh/css/element_css/header_lower.css',
+    '../../../assets-bosh/css/element_css/menu_sidebar.css',
+    '../../../assets-bosh/css/element_css/menu.css',
+    '../../../assets-bosh/css/element_css/mobile_menu.css',
+    '../../../assets-bosh/css/element_css/banner.css',
+    '../../../assets-bosh/css/element_css/cta_section.css',
+    '../../../assets-bosh/css/element_css/feature_block_one.css',
+    '../../../assets-bosh/css/element_css/product_block_one.css',
+    '../../../assets-bosh/css/element_css/clients_one.css',
+    '../../../assets-bosh/css/element_css/newsletter_one.css',
+    '../../../assets-bosh/css/element_css/main_footer_one.css',
+    '../../../assets-bosh/css/element_css/footer_bottom_one.css',
+    '../../../assets-bosh/css/element_css/scroll_to_top.css',
+    '../../../assets-bosh/css/element_css/week_sale.css',
+    '../../../assets-bosh/css/element_css/add.css',
+    '../../../assets-bosh/css/element_css/brand.css',
+    '../../../assets-bosh/css/element_css/blog.css',
+    '../../../assets-bosh/css/responsive.css',
+    '../../../assets-bosh/css/style.css',
+  ]
+})
+export class ClientProductListComponent {
+  currentActiveMenu = "products";
+}
